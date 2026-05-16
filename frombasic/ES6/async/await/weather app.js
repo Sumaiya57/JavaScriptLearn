cdccb754d28e501d8d1dfd819ada53a5f15f0f1e
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-05-16 15:34:36 +0600

    bug fix

---
+++ frombasic/ES6/async/await/weather app.js
@@ -0,0 +1,28 @@
+/*Challenge 6:
+
+Create a fake “weather app”:
+
+getCity() → returns city after 1 sec
+getWeather(city) → returns weather after 2 sec
+
+*/
+//Challenge 6:
+function getCity(){  
+   return new Promise((resolve)=>{
+    setTimeout(()=>{ 
+    resolve(`City:${"Chattogram"}`)},1000); });
+}
+function getWeather(city){  
+      return new Promise((resolve)=>{
+          setTimeout(()=>{ 
+            resolve(`Weather:${"32°C Sunny"}`)},2000); });
+}
+
+async function showdata(){  
+      const result = await getCity();
+       console.log(result);
+      const result1 = await getWeather(result);
+       console.log(result1);
+  }
+
+showdata();
