35b5e5c37888891f0cca082d769e27b1d15a4009
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-05-19 15:42:56 +0600

    add department, semester and new method isPassed()

--- OOP/fundamentals/Student_management_system/Mini_project.cpp
+++ OOP/fundamentals/Student_management_system/Mini_project.cpp
@@ -5,32 +5,50 @@ class student{
     string name;
     int id;
     float cgpa;
-    student(string n, int i, float c){
+    string dept_name;
+    int semester;
+    student(string n, int i, float c, string d, int s){
         name=n;
         id=i;
         cgpa=c;
+        dept_name=d;
+        semester=s;
     }
     void display(){
         cout << "\nStudent Information" << endl;
         cout<<"Name: "<<name<<endl;
         cout<<"ID: "<<id<<endl;
         cout<<"CGPA: "<<cgpa<<endl;
+        cout<<"Department: "<<dept_name<<endl;
+        cout<<"Semester: "<<semester<<endl;
+    }
+    void isPassed(){
+        if(cgpa>=2.0){
+            cout<<"Status: Passed"<<endl;
+        }else{
+            cout<<"Status: Failed"<<endl;
+        }
     }
 };
 int main(){
     string n;
     int i;
     float c;
-    cin>>n>>i>>c;
-    student s1(n, i, c);
-    
-    cin>>n>>i>>c;
-    student s2(n,i,c);
-    cin>>n>>i>>c;
-    student s3(n,i,c);
-    
+    string d;
+    int s;
+    cin>>n>>i>>c>>d>>s;
+    student s1(n, i, c, d, s);
+
+    cin>>n>>i>>c>>d>>s;
+    student s2(n,i,c,d,s);
+    cin>>n>>i>>c>>d>>s;
+    student s3(n,i,c,d,s);
+
     s1.display();
     s2.display();
     s3.display();
+    s1.isPassed();
+    s2.isPassed();
+    s3.isPassed();
     return 0;
 }
