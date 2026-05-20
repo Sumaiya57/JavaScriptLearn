30dd2187843bb754b469061aae3e153d3d8253c5
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-05-20 20:40:31 +0600

    modify code

--- OOP/Core_Pillars/POLYMORPHISM/Employee_management.js
+++ OOP/Core_Pillars/POLYMORPHISM/Employee_management.js
@@ -7,7 +7,7 @@ class Employee {
 class Manager extends Employee {
     
     calculateSalary() {
-        console.log("Calculating salary for manager.");
+        console.log("Calculating salary for manager:");
         return 50000;
     }
 }
@@ -15,35 +15,69 @@ class Manager extends Employee {
 class Developer extends Employee {
 
     calculateSalary() {
-        console.log("Calculating salary for developer.");
+        console.log("Calculating salary for developer:");
         return 40000;
     }
 }
 class Intern extends Employee {
     
     calculateSalary() {
-        console.log("Calculating salary for intern.");
+        console.log("Calculating salary for intern:");
         return 20000;
+    }
+}
+class Designer extends Employee {
+
+    calculateSalary() {
+        console.log("Calculating salary for designer:");
+        return 35000;
+    }
+}
+class tester extends Employee {
+
+    calculateSalary() {
+        console.log("Calculating salary for tester:");
+        return 30000;
     }
 }
 // Main Part
 
-const employees = new Employee();
-const manager = new Manager();
-const developer = new Developer();
-const intern = new Intern();
-employees.calculateSalary();
-manager.calculateSalary();
-developer.calculateSalary();
-intern.calculateSalary();
+const employees = [
+    new Manager(),
+    new Developer(),
+    new Intern(),
+    new Designer(),
+    new tester()
+];
+
+for (let emp of employees){
+    console.log(emp.calculateSalary());
+}
+//vs
+// const employees = new Employee();
+// const manager = new Manager();
+// const developer = new Developer();
+// const intern = new Intern();
+// const designer = new Designer();
+// const tester = new tester();
+// employees.calculateSalary();
+// manager.calculateSalary();
+// developer.calculateSalary();
+// intern.calculateSalary();
+// designer.calculateSalary();
+// tester.calculateSalary();
 
 //vs 
 
-const salary = employees.calculateSalary();
-console.log(salary);
-const managerSalary = manager.calculateSalary();
-console.log(managerSalary);
-const developerSalary = developer.calculateSalary();
-console.log(developerSalary);
-const internSalary = intern.calculateSalary();
-console.log(internSalary);
+// const salary = employees.calculateSalary();
+// console.log(salary);
+// const managerSalary = manager.calculateSalary();
+// console.log(managerSalary);
+// const developerSalary = developer.calculateSalary();
+// console.log(developerSalary);
+// const internSalary = intern.calculateSalary();
+// console.log(internSalary);
+// const designerSalary = designer.calculateSalary();
+// console.log(designerSalary);
+// const testerSalary = tester.calculateSalary();
+// console.log(testerSalary);
