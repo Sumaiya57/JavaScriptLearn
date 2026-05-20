00add44372fdc87bdd3b2dff01084bf105fe133c
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-05-20 20:00:01 +0600

    override method

---
+++ OOP/Core_Pillars/POLYMORPHISM/Employee_management.js
@@ -0,0 +1,38 @@
+class Employee {
+    calculateSalary(){
+        console.log("Calculating salary for employee.");
+        return 0;
+    }
+}
+class Manager extends Employee {
+    
+    calculateSalary() {
+        console.log("Calculating salary for manager.");
+        return 50000;
+    }
+}
+
+class Developer extends Employee {
+
+    calculateSalary() {
+        console.log("Calculating salary for developer.");
+        return 40000;
+    }
+}
+class Intern extends Employee {
+    
+    calculateSalary() {
+        console.log("Calculating salary for intern.");
+        return 20000;
+    }
+}
+// Main Part
+
+const employees = new Employee();
+const manager = new Manager();
+const developer = new Developer();
+const intern = new Intern();
+employees.calculateSalary();
+manager.calculateSalary();
+developer.calculateSalary();
+intern.calculateSalary();
