5938c1f3599637e9073bd8db22b81e3af0fe7ece
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-05-23 20:07:33 +0600

    use in inheritance , method overriding , polymorphism

---
+++ OOP/Core_Pillars/ABSTRACTION/Employee.js
@@ -0,0 +1,46 @@
+class Employee {
+    constructor(name, salary) {
+        this.name = name;
+        this.Salary = salary;
+    }
+
+    salary() {
+        return this.Salary;
+    }
+}
+class developer extends Employee {
+    constructor(name, salary) {
+        super(name, salary);
+    }
+    salary() {
+        console.log("Calculating salary for developer:");
+        return this.Salary;
+    }
+}
+class manager extends Employee {
+    constructor(name, salary) {
+        super(name, salary);
+    }
+    salary() {
+        console.log("Calculating salary for manager:");
+        return this.Salary;
+    }
+}
+class intern extends Employee {
+    constructor(name, salary) {
+        super(name, salary);
+    }
+    salary() {
+        console.log("Calculating salary for intern:");
+        return this.Salary;
+    }
+}
+
+const emp1 = new developer("Alice", 40000);
+const emp2 = new manager("Bob", 50000);
+const emp3 = new intern("Charlie", 20000);
+const employees = [emp1, emp2, emp3];
+for (let emp of employees) {
+    console.log(emp.name);
+    console.log(emp.salary());
+}
